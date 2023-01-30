b94c5116119caa6516b322b11bf28a45bc0f9fef
brandonpille <brandon@threefold.tech>
2023-01-30 09:39:09 +0100

    It is compiling now

--- farmerbot/factory/factory.v
+++ farmerbot/factory/factory.v
@@ -2,28 +2,11 @@ module factory
 
 import freeflowuniverse.baobab
 import freeflowuniverse.baobab.actions
-import freeflowuniverse.baobab.actionrunner
-import freeflowuniverse.baobab.processor
 import freeflowuniverse.crystallib.pathlib
 import threefoldtech.farmerbot.system
 import threefoldtech.farmerbot.manager
 
-import log
 import regex
-
-pub fn run(mut db &system.DB)! {
-	// TODO: Timur needs to make some changes:
-
-	// mut b := baobab.new()!
-	// mut resource_manager := manager.ResourceManager{}
-	// mut power_manager := manager.PowerManager{}
-	// mut ar := actionrunner.new(b.client, [&resource_manager, &power_manager])!
-	// mut processor := processor.Processor{}
-
-	// // concurrently run actionrunner, processor, and external client
-	// spawn (&ar).run()
-	// spawn (&processor).run()
-}
 
 pub fn run(path0 string) !system.DB {
 	mut logger := system.logger()
--- start.v
+++ start.v
@@ -1,5 +1,8 @@
 module main
 
+//import freeflowuniverse.baobab
+//import freeflowuniverse.baobab.actionrunner
+//import freeflowuniverse.baobab.processor
 import threefoldtech.farmerbot.system
 import threefoldtech.farmerbot.factory
 
@@ -14,5 +17,18 @@ fn main() {
 		logger.error("$err")
 		return
 	}
+
 	logger.info("$db")
+
+	// TODO: Timur needs to make some changes:
+
+	// mut b := baobab.new()!
+	// mut resource_manager := manager.ResourceManager{}
+	// mut power_manager := manager.PowerManager{}
+	// mut ar := actionrunner.new(b.client, [&resource_manager, &power_manager])!
+	// mut processor := processor.Processor{}
+
+	// // concurrently run actionrunner, processor, and external client
+	// spawn (&ar).run()
+	// spawn (&processor).run()
 }
