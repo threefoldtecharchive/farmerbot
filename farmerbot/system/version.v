ac9b495c53a6a57314db7eb623c5b8a550b4cb88
brandonpille <brandon@threefold.tech>
2023-03-16 09:41:34 +0100

    v0.1.0-rc11

--- farmerbot/system/version.v
+++ farmerbot/system/version.v
@@ -4,7 +4,7 @@ pub const (
 	version_major = u8(0)
 	version_minor = u8(1)
 	version_patch = u8(0)
-	extension = "-rc10"
+	extension = "-rc11"
 
 	version = "${version_major}.${version_minor}.${version_patch}${extension}"
 )
