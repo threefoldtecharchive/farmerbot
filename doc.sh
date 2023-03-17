346dab418e60b54e23de6f5060054d62b4bcd897
brandonpille <brandon@threefold.tech>
2023-03-17 15:53:37 +0100

    fixed doc.sh

--- doc.sh
+++ doc.sh
@@ -2,7 +2,7 @@ set -ex
 rm -rf docs/_docs
 v fmt -w farmerbot
 pushd farmerbot
-v doc -m -f html farmerbot/ -readme -comments -no-timestamp 
+v doc -m -f html . -readme -comments -no-timestamp 
 popd
 mv farmerbot/_docs docs
 
