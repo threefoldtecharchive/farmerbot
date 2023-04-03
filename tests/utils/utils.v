a6912b713d2daa999caa75a7f3f06a88d7d11d3d
brandonpille <brandon@threefold.tech>
2023-04-03 15:25:35 +0200

    revert utils.v

--- tests/utils/utils.v
+++ tests/utils/utils.v
@@ -103,12 +103,13 @@ pub fn (mut t TestEnvironment) run(name string, test Test) ! {
 	f.processor.reset() or {
 		return error("Failed resetting processor: $err")
 	}
-	c.reset() or {
-		return error("Failed resetting client: $err")
-	}
 
 	t_ar := spawn (&f.actionrunner).run()
 	t_pr := spawn (&f.processor).run()
+
+	c.reset() or {
+		return error("Failed resetting client: $err")
+	}
 
 	test(mut f, mut c) or {
 		f.processor.running = false
