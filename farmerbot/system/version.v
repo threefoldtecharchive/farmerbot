15f7c3ad49e33ab6b7f9c89a357e87a1778422d0
brandonpille <brandon@threefold.tech>
2023-06-20 15:06:49 +0200

    change release version 0.2.0-rc1

--- farmerbot/system/version.v
+++ farmerbot/system/version.v
@@ -2,9 +2,9 @@ module system
 
 pub const (
 	version_major = u8(0)
-	version_minor = u8(1)
+	version_minor = u8(2)
 	version_patch = u8(0)
-	extension     = ''
+	extension     = '-rc1'
 
 	version       = '${version_major}.${version_minor}.${version_patch}${extension}'
 )
