ad26c2ea2100cded7be0e7f87c0156209858e271
brandonpille <brandon@threefold.tech>
2023-07-28 08:40:52 +0200

    New patch version v0.2.1

--- farmerbot/system/version.v
+++ farmerbot/system/version.v
@@ -3,7 +3,7 @@ module system
 pub const (
 	version_major = u8(0)
 	version_minor = u8(2)
-	version_patch = u8(0)
+	version_patch = u8(1)
 	extension     = ''
 
 	version       = '${version_major}.${version_minor}.${version_patch}${extension}'
